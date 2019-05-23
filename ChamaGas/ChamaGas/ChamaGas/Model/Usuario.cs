160274ba8212c56f271b27f3e70399986ab763b6
leonardo.hbusolo <leonardo.hbusolo@SJR0577158W10-1>
2019-05-22 22:10:00 -0300

    commit 22/05/2019 22:09

---
+++ ChamaGas/ChamaGas/ChamaGas/Model/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChamaGas.Model
+{
+    public class Usuario
+    {
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
--- ChamaGas/ChamaGas/ChamaGas/View/LoginView.xaml.cs
+++ ChamaGas/ChamaGas/ChamaGas/View/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChamaGas.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,17 @@ namespace ChamaGas.View
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginView : ContentPage
 	{
+        Usuario usuario { get; set; };
 		public LoginView ()
 		{
 			InitializeComponent ();
+            usuario = new Usuario();
+            this.BindingContext = usuario;
 		}
-	}
+
+        private void BtnEntrar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Informações", $"E-mail:{usuario.Email} Senha {usuario.Senha}", "Fechar");
+        }
+    }
 }
