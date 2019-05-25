2f5d874980b7015d6e07cd8af54242633c165a95
leonardo.hbusolo <leonardo.hbusolo@SJR0577158W10-1>
2019-05-24 22:23:58 -0300

    Servicer 1.0.0

---
+++ ChamaGas/ChamaGas/ChamaGas/Services/ReqRes_Services.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamaGas.Services
+{
+    public class ReqRes_Services : Base_Services
+    {
+        protected const string base_url = "https://reqres.in/";
+        string api_name;
+
+        public ReqRes_Services(string api_name) : base(base_url)
+        {
+            this.api_name = api_name;
+        }
+
+        public override async Task<T> Get<T>(string input)
+        {
+            var client = base.GetClient();
+            var retorno = await client.GetAsync($"{api_name}?page={input}");
+            var retornoTexto = await retorno.Content.ReadAsStringAsync();
+            T obj_Convertido = JsonConvert.DeserializeObject<T>(retornoTexto);
+            return obj_Convertido;
+        }
+
+    }
+}
--- ChamaGas/ChamaGas/ChamaGas/View/PessoaView.xaml.cs
+++ ChamaGas/ChamaGas/ChamaGas/View/PessoaView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChamaGas.Model;
+using ChamaGas.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,31 @@ namespace ChamaGas.View
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PessoaView : ContentPage
 	{
-		public PessoaView ()
+        Base_Services clientCep = new Base_Services(Base_Services.URL_VIACEP);
+
+        ReqRes_Services clientReqRes = new ReqRes_Services("api/users");
+
+
+        //Base_Services clientApi = new Base_Services(Base_Services.URL_API);
+        public PessoaView ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async void EtCep_Unfocused(object sender, FocusEventArgs e)
+        {
+            var cep_ret = await clientCep.Get<CEP>(etCep.Text);
+
+            this.etCep.Text = cep_ret.cep;
+            this.etLogradouro.Text = cep_ret.logradouro;
+            this.etComplemento.Text = cep_ret.complemento;
+            this.etBairro.Text = cep_ret.bairro;
+            this.etLocalidade.Text = cep_ret.localidade;
+            this.etUf.Text = cep_ret.uf;
+
+            var obj_reqres = await clientReqRes.Get<RetornoTeste>("2");
+            await DisplayAlert("Informações", $"Id:{obj_reqres.data.FirstOrDefault().id.ToString()}, " +
+                $" Email:{obj_reqres.data.FirstOrDefault().email.ToString()}", "Fechar");
+        }
+    }
 }
